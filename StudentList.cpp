8f1dc41e8a0eee41d2fc79a775769b440857debe
ew6803 <39627531+ew6803@users.noreply.github.com>
2019-10-15 10:45:47 -0700

    Add files via upload

---
+++ StudentList.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstring>
+#include <sstream>
+
+using namespace std;
+
+struct students {
+  cstring firstName;
+  cstring lastName;
+  double GPA;
+  int iD;
+} example;
+
+void printNames(students);
+void addName(students);
+void deleteName(students);
+
+
+int main() {
+  cstring input;
+  vector<struct> StudentList;
+  cout << "Welcome to Student List, ADD, PRINT, DELETE, QUIT";
+  cin << input;
+  if (input 
+
+  
+  return 0;
+}
+
+void printNames(students) {
+
+}
+void addName(students) {
+
+}
+void deleteName(students) {
+
+} 
