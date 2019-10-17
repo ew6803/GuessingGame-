be8f59745758536e33caeee853dabf5b170de80b
ew6803 <39627531+ew6803@users.noreply.github.com>
2019-10-17 10:11:52 -0700

    Add files via upload

--- StudentList.cpp
+++ StudentList.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <cstring>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
 struct students {
-  cstring firstName;
-  cstring lastName;
   double GPA;
   int iD;
-} example;
+};
 
 void printNames(students);
 void addName(students);
@@ -17,13 +16,32 @@ void deleteName(students);
 
 
 int main() {
-  cstring input;
-  vector<struct> StudentList;
-  cout << "Welcome to Student List, ADD, PRINT, DELETE, QUIT";
-  cin << input;
-  if (input 
+  char input[10];
+  bool onQuit = false; 
+  vector<students*> StudentList;
+  while (onQuit == false) {
+  cout << "Welcome to Student List, ADD, PRINT, DELETE, QUIT\n";
+  cin.getline (input, 10); 
+  if (strcmp(input, "QUIT") == 0) {
+    onQuit = true;
+  }
+  else if (strcmp(input, "ADD") == 0) {
+    // addName(StudentList*);
+    cout << "\ntest";
+  }
+  else if (strcmp(input, "PRINT") == 0) {
+    // printNames(StudentList*);
+    cout << "\ntest";
+  }
+  else if (strcmp(input, "DELETE") == 0) {
+    cout << "\ntest";
+    // deleteName(StudentList*);
+  }
+  else {
+    cout << "\nPlease Enter a Valid Command, ADD, DELETE, PRINT, QUIT";
+  }
 
-  
+  }  
   return 0;
 }
 
